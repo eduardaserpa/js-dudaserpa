88430da907a2bfcddc21d149e3673d87cf0c16e2
eduardaserpa <eduardaserpamandetta1612@gmail.com>
2024-08-21 15:41:07 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+fanction setup( ) }
+createcanvas(600.600)
+  background(black)
+}
+
+function draw() {
+stroke ("blue");
+  fill ("red")
+  
+  if(mouseIsprossed) {
+    rect(mouseX, mouseY , 20 , 35,);
+  }
+  
+} 
